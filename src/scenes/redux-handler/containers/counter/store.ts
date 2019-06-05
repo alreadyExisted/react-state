029b30db1663448b3d5b3e2642c43375d56cb17a
alreadyExisted <vit.polyanskiy@gmail.com>
2019-06-06 00:11:23 +0300

    fixed redux-handler async action

--- src/scenes/redux-handler/containers/counter/store.ts
+++ src/scenes/redux-handler/containers/counter/store.ts
@@ -1,8 +1,8 @@
 import { sync, rx } from 'redux-handler'
-import { of } from 'rxjs'
-import { concatMap, timeout } from 'rxjs/operators'
 import { CounterStore } from 'store'
 import { handler } from 'scenes/redux-handler/handler'
+import { timer } from 'rxjs'
+import { concat } from 'rxjs/operators'
 
 export const counterHandler = handler<CounterStore>({
   value: 0
@@ -14,11 +14,7 @@ export const incCounter = counterHandler
 
 export const incCounterAsync = counterHandler
   .action('INCREMENT_COUNTER_ASYNC')
-  .pipe(
-    rx(() =>
-      of(1000).pipe(concatMap(duration => [timeout(duration), incCounter()]))
-    )
-  )
+  .pipe(rx(() => timer(1000).pipe(concat([incCounter()]))))
 
 export const decCounter = counterHandler
   .action('DECREMENT_COUNTER')
