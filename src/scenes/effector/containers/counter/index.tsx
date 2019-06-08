d398a1c3708408dbe77c5b340fb901e3ba7fabf3
alreadyExisted <vit.polyanskiy@gmail.com>
2019-06-08 13:50:24 +0300

    fixed hooks errors

--- src/scenes/effector/containers/counter/index.tsx
+++ src/scenes/effector/containers/counter/index.tsx
@@ -3,19 +3,29 @@ import { createComponent } from 'effector-react'
 import { Counter } from 'scenes/@components'
 import { incCounter, decCounter, incCounterAsync, counterStore } from './store'
 
-export const CounterContainer = createComponent(
+interface OwnProps {}
+
+interface InjectedProps extends OwnProps {
+  counter: number
+}
+
+export function CounterContainerComponent({ counter }: InjectedProps) {
+  const incValue = useCallback(() => incCounter(), [])
+  const incValueAsync = useCallback(() => incCounterAsync({}), [])
+  const decValue = useCallback(() => decCounter(), [])
+  return (
+    <Counter
+      counter={counter}
+      incValue={incValue}
+      incValueAsync={incValueAsync}
+      decValue={decValue}
+    />
+  )
+}
+
+export const CounterContainer = createComponent<OwnProps, InjectedProps>(
   counterStore,
-  (_, { counter }) => {
-    const incValue = useCallback(() => incCounter(), [])
-    const incValueAsync = useCallback(() => incCounterAsync({}), [])
-    const decValue = useCallback(() => decCounter(), [])
-    return (
-      <Counter
-        counter={counter}
-        incValue={incValue}
-        incValueAsync={incValueAsync}
-        decValue={decValue}
-      />
-    )
-  }
+  (props, { counter }) => (
+    <CounterContainerComponent {...props} counter={counter} />
+  )
 )
